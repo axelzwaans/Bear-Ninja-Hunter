897333718544a5999bd287fe902632ea767543ab
Axel Zwaans <axelzwaans@hotmail.com>
2022-06-06 18:27:32 +0000

    Add function for getResult

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,61 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     getResult()
 }))
 
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3 + 1)
+
+    if (randomNumber === 1) {
+        computerChoice = 'bear'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'ninja'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'hunter'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === playerChoice) {
+        result = "It's a draw!"
+    }
+    if (computerChoice === 'bear' && playerChoice === 'ninja') {
+        result = "you died!"
+        incrementLose()
+    }
+    if (computerChoice === 'bear' && playerChoice === 'hunter') {
+        result = "You lived!"
+        incrementScore()
+    }
+    if (computerChoice === 'ninja' && playerChoice === 'hunter') {
+        result = "You died!"
+        incrementLose()
+    }
+    if (computerChoice === 'ninja' && playerChoice === 'bear') {
+        result = "You lived!"
+        incrementScore()
+    }
+    if (computerChoice === 'hunter' && playerChoice === 'bear') {
+        result = "You died!"
+        incrementLose()
+    }
+    if (computerChoice === 'hunter' && playerChoice === 'ninja') {
+        result = "You lived!"
+        incrementScore()
+    }
+    resultDisplay.innerHTML = result
+}
+
+function incrementScore() {
+    
+    let oldscore = parseInt(document.getElementById('score').innerText)
+    document.getElementById('score').innerText = ++oldScore
+
+}
+
+function incrementLose() {
+
+    let oldScore = parseInt(document.getElementById('died').innerText)
+    document.getElementById('died').innerText = ++oldScore
+}
